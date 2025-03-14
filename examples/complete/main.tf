04a66edf2e0db2838b5c3455aa7b7ebe9579c9e3
shanye <shanye.xzq@alibaba-inc.com>
2025-03-14 10:52:31 +0800

    examples/complete: improve the examples

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -6,7 +6,10 @@ data "alicloud_images" "default" {
 }
 
 data "alicloud_instance_types" "default" {
-  availability_zone = data.alicloud_zones.default.zones[0].id
+  availability_zone    = data.alicloud_zones.default.zones[0].id
+  cpu_core_count       = 2
+  memory_size          = 8
+  instance_type_family = "ecs.g6"
 }
 
 data "alicloud_resource_manager_resource_groups" "default" {
